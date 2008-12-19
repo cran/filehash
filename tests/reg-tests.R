5ad47ba386c68dafa590304f74cbfb8d440f82a9
Roger D. Peng <rpeng@jhsph.edu>
2008-12-19 00:00:00 +0000

    version 2.0-1

--- tests/reg-tests.R
+++ tests/reg-tests.R
@@ -170,3 +170,14 @@ dbList(db)
 dbExists(db, "a")
 
 file.remove("mydb")
+
+################################################################################
+## Check queue
+
+db <- createQ("testq")
+push(db, 1)
+push(db, 2)
+top(db)
+
+pop(db)
+top(db)
